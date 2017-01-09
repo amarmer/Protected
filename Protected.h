198c947a370a946f983ad7776d498197d273efc3
Alex Marmer <amarmer@users.noreply.github.com>
2017-01-09 12:54:57 -0800

    Update Protected.h

--- Protected.h
+++ Protected.h
@@ -1,24 +1,3 @@
-/*
-When a data variable needs to be synchronized, in addition to the data variable, added synchronization variable (like mutex, etc.).
-Class Protected combines these 2 variables and doesn't allow to use the data variable without synchronization.
-
-For instance:
-
-// Methods of data cannot accessed since Protected uses private inheritance.
-ProtectedByCriticalSection<vector<int>> protectedData = {1, 2, 3};
-
-// In order to access methods, should be called 'Lock()', it returns 'Locker' object, which allow to access data via it's pointer.
-// Constructor of 'Locker' locks synchronization object and it's destructor unlocks it.
-auto pData = protectedData.Lock();
-
-pData->push_back(1);
-
-// Or could be used dereference to data
-auto& data = *pData;
-
-data.push_back(1);
-*/
-
 #pragma once
 
 #include <mutex>
